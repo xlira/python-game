bb4a2f42d5b1f0abf5c0b973cb3d347e9d99db1e
Lucas <lucas.lsilvalr@gmail.com>
2022-04-25 00:57:37 -0300

    Foi feita a adição de um laço de repetição, a inserção de um numero aleatorio não fixo e um sistema de pontos.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,20 +1,52 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 2
+rodada = 1
+pontos = 1000
 
-chute_bruto = input("Digite um numero: ")
+print("Qual o nível de dificuldade?")
+print("(1) Facíl  (2) Médio  (3) Difícil")
 
-chute = int(chute_bruto)
-
-print("Você digitou: ", chute)
-
-if (chute == numero_secreto):
-    print("Você acertou!")
+nivel = int(input("Defina o nível:"))
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
 else:
-    if(chute > numero_secreto):
-        print("Você errou! O numero secreto é menor que o numero inserido")
-    if(chute < numero_secreto):
-        print("Você errou! O numero secreto é maior que o numero inserido")
+    total_de_tentativas =5
+
+while (total_de_tentativas > 0 ):
+    print("Rodada:", rodada)
+    print("Total de tentavidas: {}".format( total_de_tentativas))
+    chute_bruto = input("Digite um numero: ")
+    print ("Vocêr digitou:", chute_bruto)
+    chute = int(chute_bruto)
+
+    if(chute < 1 or chute >100):
+        print("Você precisa indicar um numero entre 1 e 100")
+        continue
+
+    if (chute == numero_secreto):
+        print("Você acertou! Sua pontuação foi {} pontos!".format(pontos))
+        break
+
+    if (total_de_tentativas < 1):
+        print("As tentativas acabaram!")
+        break
+
+    else:
+        if(chute > numero_secreto):
+            print("Você errou! O numero secreto é menor que o numero inserido!")
+        if(chute < numero_secreto):
+            print("Você errou! O numero secreto é maior que o numero inserido"!)
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
+    total_de_tentativas = total_de_tentativas - 1
+    rodada = rodada + 1
 print("Fim do jogo!")
