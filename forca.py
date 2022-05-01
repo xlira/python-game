c304ad58af5f37f73e09dce042f8ff3e10b97163
Lucas <lucas.lsilvalr@gmail.com>
2022-05-01 18:25:01 -0300

    encerramento do jogo forca

--- forca.py
+++ forca.py
@@ -1,3 +1,4 @@
+import random
 
 def jogar():
     print("*********************************")
@@ -6,21 +7,45 @@ def jogar():
 
     print("Jogo da Forca.")
 
-    palavra = 'banana'
+    arquivo = open("palavras.txt", "r")
+    palavras= []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra = palavras[numero].upper()
+
+    print(palavra)
+    letras_acertadas = ["_" for letra in palavra]
+
     acertou = False
     forcou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while ( not acertou and not forcou):
 
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra:
-            if (chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}.".format(letra, index))
-            index = index + 1
+        if(chute in palavra):
+            index = 0
+            for letra in palavra:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                    print("Encontrei a letra {} na posição {}.".format(letra, index))
+                index = index + 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
 
 if(__name__ == "__main__"):
