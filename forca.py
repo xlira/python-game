5a752e8647076abda9d813dc83e7ccc3709bf912
Lucas <lucas.lsilvalr@gmail.com>
2022-05-02 20:30:43 -0300

    Polindo o codigo com funções e adicionando design ao jogo de forca

--- forca.py
+++ forca.py
@@ -1,14 +1,53 @@
 import random
 
 def jogar():
+
+    imprime_inicio()
+    palavra = carregar_palavra()
+
+    print(palavra)
+    letras_acertadas = ["_" for letra in palavra]
+
+    acertou = False
+    forcou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while ( not acertou and not forcou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra):
+            index = 0
+            for letra in palavra:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                    print("Encontrei a letra {} na posição {}.".format(letra, index))
+                index = index + 1
+        else:
+            erros += 1
+            desenha_forca(erros)
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        print(imprime_mensagem_vencedor())
+    else:
+        print(imprime_mensagem_perdedor(palavra))
+
+
+def imprime_inicio():
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    print("Jogo da Forca.")
-
+def definir_palavra_secreta():
     arquivo = open("palavras.txt", "r")
-    palavras= []
+    palavras = []
 
     for linha in arquivo:
         linha = linha.strip()
@@ -22,31 +61,105 @@ def jogar():
     print(palavra)
     letras_acertadas = ["_" for letra in palavra]
 
-    acertou = False
-    forcou = False
-    erros = 0
 
-    print(letras_acertadas)
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
 
-    while ( not acertou and not forcou):
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
 
-        chute = input("Qual a letra?")
-        chute = chute.strip().upper()
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
 
-        if(chute in palavra):
-            index = 0
-            for letra in palavra:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                    print("Encontrei a letra {} na posição {}.".format(letra, index))
-                index = index + 1
-        else:
-            erros += 1
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
 
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
 
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def carregar_palavra():
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra = palavras[numero].upper()
+    return palavra
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprime_mensagem_perdedor(palavra):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
 if(__name__ == "__main__"):
     jogar()
